c000bd540365f6c22e950c1b4374519c8f8c7809
juliusbuckley <juliusbuckley@gmail.com>
2017-01-15 10:24:06 -0800

    Delete commented out code

--- server/utils/createTokenList.js
+++ server/utils/createTokenList.js
@@ -7,20 +7,6 @@ import { serialize } from './tokenFunctions';
 import appendToFile from '../appendToFile';
 mongoose.Promise = require('bluebird');
 
-// Client.find({}).exec()
-//   .then(clients => {
-//     clients.forEach(client => {
-//       Item.find({'_client': client._id}).exec()
-//         .then(items => {
-//           items.forEach(item => {
-//             appendToFile('tokenList.txt', serialize(client._id, item._id, item.institution_type));
-//           });
-//         })
-//         .catch(err => console.log(`Item not in db: ${err}`));
-//     });
-//   })
-//   .catch(err => console.log(`Client not in db: ${err}`));
-
 Client.find({}).exec()
   .then(clients => {
     return Promise.all(clients.map(client => {
