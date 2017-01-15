0b934393ad05ad994316601d8616a876c01b5b96
juliusbuckley <juliusbuckley@gmail.com>
2017-01-15 10:12:24 -0800

    Make appendFile async and added lodash

--- server/appendToFile.js
+++ server/appendToFile.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
 const appendToFile = (filename, line) => {
-  fs.appendFileSync(filename, `${line}\n`, (err) => {
+  fs.appendFile(filename, `${line}\n`, (err) => {
     if (err) {
       throw err;
     }
--- server/utils/createTokenList.js
+++ server/utils/createTokenList.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import db from '../config/db';
 import mongoose from 'mongoose';
 import Client from '../models/Client';
@@ -6,16 +7,30 @@ import { serialize } from './tokenFunctions';
 import appendToFile from '../appendToFile';
 mongoose.Promise = require('bluebird');
 
+// Client.find({}).exec()
+//   .then(clients => {
+//     clients.forEach(client => {
+//       Item.find({'_client': client._id}).exec()
+//         .then(items => {
+//           items.forEach(item => {
+//             appendToFile('tokenList.txt', serialize(client._id, item._id, item.institution_type));
+//           });
+//         })
+//         .catch(err => console.log(`Item not in db: ${err}`));
+//     });
+//   })
+//   .catch(err => console.log(`Client not in db: ${err}`));
+
 Client.find({}).exec()
   .then(clients => {
-    clients.forEach(client => {
-      Item.find({'_client': client._id}).exec()
-        .then(items => {
-          items.forEach(item => {
-            appendToFile('tokenList.txt', serialize(client._id, item._id, item.institution_type));
-          });
-        })
+    return Promise.all(clients.map(client => {
+      return Item.find({'_client': client._id}).exec()
         .catch(err => console.log(`Item not in db: ${err}`));
-    });
+    }));
   })
-  .catch(err => console.log(`Client not in db: ${err}`));
+  .then(clientItems => {
+    const items = _.flatten(clientItems);
+    const strings = items.map(item => serialize(item._client, item._id, item.institution_type));
+    appendToFile('tokenList.txt', strings.join('\n'));
+  })
+  .catch(console.error.bind(console));
